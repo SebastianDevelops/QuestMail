16352edda8388bcfe2fefb88ed6d79fdc204fd5e
SebastianDevelops <sebastiandevelops@gmail.com>
2025-05-26 20:29:58 +0200

    Set connection string variable

--- MedPulse/Program.cs
+++ MedPulse/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("./etc/secrets/appsettings.json", optional: true, reloadOnChange: true)
@@ -15,7 +16,6 @@ builder.Configuration
     .AddJsonFile($"./etc/secrets/appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
     .AddEnvironmentVariables();
 // Add services to the container.
-Console.WriteLine("The base directory is: " + (AppContext.BaseDirectory.StartsWith("/") ? AppContext.BaseDirectory.Substring(1) : AppContext.BaseDirectory));// Register Settings as a singleton
 builder.Services.Configure<Settings>(builder.Configuration.GetSection("Settings"));
 builder.Services.AddSingleton(sp =>
     sp.GetRequiredService<IOptions<Settings>>().Value);
@@ -23,12 +23,32 @@ builder.Services.AddSingleton(sp =>
 builder.Services.Configure<AzureOpenAI>(
     builder.Configuration.GetSection("Settings"));
 
+if (builder.Environment.IsDevelopment())
+{
+    Environment.SetEnvironmentVariable("Settings.AzureOpenAI.apikey", builder.Configuration["Settings:AzureOpenAI:apikey"]);
+    Environment.SetEnvironmentVariable("Settings.AzureOpenAI.model", builder.Configuration["Settings:AzureOpenAI:model"]);
+    Environment.SetEnvironmentVariable("Settings.AzureOpenAI.endpoint", builder.Configuration["Settings:AzureOpenAI:endpoint"]);
+    
+    Environment.SetEnvironmentVariable("Settings.GoogleGemini.apikey", builder.Configuration["Settings:GoogleGemini:apikey"]);
+    Environment.SetEnvironmentVariable("Settings.GoogleGemini.model", builder.Configuration["Settings:GoogleGemini:model"]);
+    
+    Environment.SetEnvironmentVariable("Settings.Pinata.JWT", builder.Configuration["Settings:Pinata:JWT"]);
+    Environment.SetEnvironmentVariable("Settings.Pinata.BaseUrl", builder.Configuration["Settings:Pinata:BaseUrl"]);
+    
+    Environment.SetEnvironmentVariable("Settings.Postmark.apikey", builder.Configuration["Settings:Postmark:apikey"]);
+    Environment.SetEnvironmentVariable("Settings.Postmark.fromEmail", builder.Configuration["Settings:Postmark:fromEmail"]);
+    Environment.SetEnvironmentVariable("Settings.Postmark.replyTo", builder.Configuration["Settings:Postmark:replyTo"]);
+    
+    Environment.SetEnvironmentVariable("DefaultConnection", builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<QuestMailContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString(Environment.GetEnvironmentVariable("DefaultConnection"))));
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
