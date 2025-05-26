ab44f9a234783b2d7ca00e9bb97e21f5e858c462
SebastianDevelops <sebastiandevelops@gmail.com>
2025-05-26 19:47:48 +0200

    use environment variables

--- MedPulse/Infrastructure/Settings.cs
+++ MedPulse/Infrastructure/Settings.cs
@@ -10,26 +10,26 @@ public class Settings
 
 public class AzureOpenAI
 {
-    public string Apikey { get; set; } = string.Empty;
-    public string Model { get; set; } = string.Empty;
-    public string Endpoint { get; set; } = String.Empty;
+    public string Apikey { get; set; } = Environment.GetEnvironmentVariable("Settings.AzureOpenAI.apikey") ?? string.Empty;
+    public string Model { get; set; } = Environment.GetEnvironmentVariable("Settings.AzureOpenAI.model") ?? string.Empty;
+    public string Endpoint { get; set; } = Environment.GetEnvironmentVariable("Settings.AzureOpenAI.endpoint") ?? string.Empty;
 }
 
 public class GoogleGemini
 {
-    public string Apikey { get; set; } = string.Empty;
-    public string Model { get; set; } = string.Empty;
+    public string Apikey { get; set; } = Environment.GetEnvironmentVariable("Settings.GoogleGemini.apikey") ?? string.Empty;
+    public string Model { get; set; } = Environment.GetEnvironmentVariable("Settings.GoogleGemini.model") ?? string.Empty;
 }
 
 public class Pinata
 {
-    public string JWT { get; set; } = String.Empty;
-    public string BaseUrl { get; set; } = String.Empty;
+    public string JWT { get; set; } =Environment.GetEnvironmentVariable("Settings.Pinata.JWT") ?? string.Empty;
+    public string BaseUrl { get; set; } = Environment.GetEnvironmentVariable("Settings.Pinata.BaseUrl") ?? string.Empty;
 }
 
 public class Postmark
 {
-    public string ApiKey { get; set; } = String.Empty;
-    public string FromEmail { get; set; } = String.Empty;
-    public string ReplyTo { get; set; }
+    public string ApiKey { get; set; } = Environment.GetEnvironmentVariable("Settings.Postmark.apikey") ?? string.Empty;
+    public string FromEmail { get; set; } = Environment.GetEnvironmentVariable("Settings.Postmark.fromEmail") ?? string.Empty;
+    public string ReplyTo { get; set; } = Environment.GetEnvironmentVariable("Settings.Postmark.replyTo") ?? string.Empty;
 }
