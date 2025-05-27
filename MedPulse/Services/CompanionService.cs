d0698ea549d8f5516a56cf5ac3c2a78fdec2585f
SebastianDevelops <sebastiandevelops@gmail.com>
2025-05-27 02:02:07 +0200

    update companion image creation

--- MedPulse/Services/CompanionService.cs
+++ MedPulse/Services/CompanionService.cs
@@ -26,8 +26,8 @@ public class CompanionService : ICompanionService
         Console.WriteLine("companion image url: "+ companion.ImageUrl);
         if (String.IsNullOrEmpty(companion.ImageUrl))
         {
-            var imageUrl = await UploadImageToPinataAsync();
-            Console.WriteLine("Image URL generated: " + imageUrl);
+            var companionChar = companion.Name.Substring(0);
+            var imageUrl = $"https://placehold.co/60x60/e0ac69/1a1a2e?text={companionChar}";
             companion.ImageUrl = imageUrl;
             await _unitOfWork.Companions.UpdateAsync(companion);
             return imageUrl;
@@ -36,26 +36,26 @@ public class CompanionService : ICompanionService
         return companion.ImageUrl;
     }
     
-    private async Task<string> UploadImageToPinataAsync()
-    {
-        Console.WriteLine("Starting image generation...");
-        var googleAi = new GoogleAi( Environment.GetEnvironmentVariable("Settings.GoogleGemini.apikey"));
-        Console.WriteLine("Google AI initialized.");
-        var imageModel = googleAi.CreateImageModel(Environment.GetEnvironmentVariable("Settings.GoogleGemini.model"));
-        Console.WriteLine("Image model created.");
-        var response = await imageModel.GenerateImagesAsync(Constants.ImageGenPrompt);
-        Console.WriteLine("Image generation completed.");
-        var image = response?.Predictions?.FirstOrDefault();
-        Console.WriteLine("Image retrieved from response.");
-        var imageBytes = image?.BytesBase64Encoded;
-        Console.WriteLine("Image bytes retrieved.");
-        
-        using (var ms = new MemoryStream(Convert.FromBase64String(imageBytes)))
-        {
-            var pinataResponse = await _pinataClient.UploadFileAsync(ms, $"{Context.CompanionName}.png");
-            Console.WriteLine("Image uploaded to Pinata.");
-            Task.Delay(2500).Wait();
-            return $"{Environment.GetEnvironmentVariable("Settings.Pinata.BaseUrl")}{pinataResponse.Data.Cid}";
-        }
-    }
+    // private async Task<string> UploadImageToPinataAsync()
+    // {
+    //     Console.WriteLine("Starting image generation...");
+    //     var googleAi = new GoogleAi( Environment.GetEnvironmentVariable("Settings.GoogleGemini.apikey"));
+    //     Console.WriteLine("Google AI initialized.");
+    //     var imageModel = googleAi.CreateImageModel(Environment.GetEnvironmentVariable("Settings.GoogleGemini.model"));
+    //     Console.WriteLine("Image model created.");
+    //     var response = await imageModel.GenerateImagesAsync(Constants.ImageGenPrompt);
+    //     Console.WriteLine("Image generation completed.");
+    //     var image = response?.Predictions?.FirstOrDefault();
+    //     Console.WriteLine("Image retrieved from response.");
+    //     var imageBytes = image?.BytesBase64Encoded;
+    //     Console.WriteLine("Image bytes retrieved.");
+    //     
+    //     using (var ms = new MemoryStream(Convert.FromBase64String(imageBytes)))
+    //     {
+    //         var pinataResponse = await _pinataClient.UploadFileAsync(ms, $"{Context.CompanionName}.png");
+    //         Console.WriteLine("Image uploaded to Pinata.");
+    //         Task.Delay(2500).Wait();
+    //         return $"{Environment.GetEnvironmentVariable("Settings.Pinata.BaseUrl")}{pinataResponse.Data.Cid}";
+    //     }
+    // }
 }
